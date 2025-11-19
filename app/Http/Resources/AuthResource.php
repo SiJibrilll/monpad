c0ad581cdf475d2f38750f66dd59db0571eccdbc
SiJibrilll <jibrilianwa@gmail.com>
2025-11-19 15:08:56 +0700

    revised auth

--- app/Http/Resources/AuthResource.php
+++ app/Http/Resources/AuthResource.php
@@ -18,6 +18,7 @@ class AuthResource extends JsonResource
     {
         return [
             'user' =>  UserResourceService::make($this->resource['user']),
+            'role' => $this->resource['user']->getRoleNames()->first(),
             'token' => $this->resource['token']
         ];
     }
